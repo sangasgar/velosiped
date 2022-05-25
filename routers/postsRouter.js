d9fbad668282c961af0fd6946b3877ac799e7238
sangasgar <sangas@yandex.ru>
2022-05-25 23:10:41 +0300

    Form register and singIn

--- routers/postsRouter.js
+++ routers/postsRouter.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+router.get('/add', (req, res) => {
+  res.render('addpost');
+});
+router.get('/', (req, res) => {
+  res.render('fullpost');
+});
+
+router.get('/post/id:', (req, res) => {
+  res.render('detailpost');
+});
 module.exports = router;
